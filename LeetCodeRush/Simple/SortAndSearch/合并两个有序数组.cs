e00c87988b2139668837c32fecd5c21d29d42f55
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-06-26 17:32:02 +0800

    Simple Sort And Search

---
+++ LeetCodeRush/Simple/SortAndSearch/合并两个有序数组.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeRush.Simple.SortAndSearch
+{
+    [TestFixture]
+    public class 合并两个有序数组
+    {
+        [Test]
+        public void TestSample1()
+        {
+            var nums1 = new[] {1, 2, 3, 0, 0, 0};
+            int m = 3;
+
+            var nums2 = new[] {2, 5, 6};
+            int n = 3;
+            new Solution().Merge(nums1,m,nums2,n);
+
+            Assert.AreEqual(new []{ 1, 2, 2, 3, 5, 6 },nums1);
+        }
+        [Test]
+        public void TestSample2()
+        {
+            var nums1 = new[] { 0};
+            int m = 0;
+
+            var nums2 = new[] { 1 };
+            int n = 1;
+            new Solution().Merge(nums1, m, nums2, n);
+
+            Assert.AreEqual(new[] { 1 }, nums1);
+        }
+        [Test]
+        public void TestSample3()
+        {
+            var nums1 = new[] { 2,0 };
+            int m = 1;
+
+            var nums2 = new[] { 1 };
+            int n = 1;
+            new Solution().Merge(nums1, m, nums2, n);
+
+            Assert.AreEqual(new[] { 1,2 }, nums1);
+        }
+        public class Solution
+        {
+            public void Merge(int[] nums1, int m, int[] nums2, int n)
+            {
+                if (m == 0)
+                {
+                    for (int k = 0; k < nums2.Length; k++)
+                    {
+                        nums1[k] = nums2[k];
+                    }
+                    return;
+                }
+                if (n == 0) return;
+                int j = n-1;
+                int i = m-1;
+                int z = m+n-1;
+                while (z>=0 && j>=0)
+                {
+                    if (i < 0)
+                    {
+                        nums1[z] = nums2[j];
+                        j--;
+
+                    }
+                    else if (nums1[i] > nums2[j])
+                    {
+                        nums1[z] = nums1[i];
+                        i--;
+                    }
+                    else
+                    {
+                        nums1[z] = nums2[j];
+                        j--;
+                    }
+                    z--;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCodeRush/Simple/SortAndSearch/第一个错误的版本.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeRush.Simple.SortAndSearch
+{
+    [TestFixture]
+    public class 第一个错误的版本
+    {
+//        [TestCase(4,ExpectedResult = 4)]
+//        [TestCase(6, ExpectedResult = 4)]
+//        [TestCase(5, ExpectedResult = 4)]
+//        [TestCase(1000, ExpectedResult = 4)]
+
+        [TestCase(2126753390, ExpectedResult = 1702766719)]
+
+        public int TestSample(int x)
+        {
+            return new Solution().FirstBadVersion(x);
+        }
+        /* The isBadVersion API is defined in the parent class VersionControl.bool IsBadVersion(int version); */
+        public abstract class VersionControl
+        {
+            public bool IsBadVersion(int version)
+            {
+                return version >= 1702766719;
+            }
+        }
+        public class Solution : VersionControl
+        {
+            public int FirstBadVersion(int n)
+            {
+                int l = 0;
+                int r = n;
+                int m = (l + r) >> 1;
+                while (m > l && m < r)
+                {
+                    if (IsBadVersion(m))
+                    {
+                        r = m;
+                    }
+                    else
+                    {
+                        l = m;
+                    }
+                    m = l + ((r - l) >> 1);
+                }
+
+                return r;
+            }
+        }
+    }
+}
