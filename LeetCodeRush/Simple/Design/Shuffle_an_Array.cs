13bf99e1020ee4957489edb08afa566c6d0f21ac
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-06-29 09:02:20 +0800

    Simple Design

---
+++ LeetCodeRush/Simple/Design/Shuffle_an_Array.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeRush.Simple.Design
+{
+    [TestFixture]
+    public class Shuffle_an_Array
+    {
+        public class Solution
+        {
+            private readonly int[] original = null;
+            public Solution(int[] nums)
+            {
+                original = nums;
+            }
+
+            /** Resets the array to its original configuration and return it. */
+            public int[] Reset()
+            {
+                if (original == null) return null;
+                var reset = new int[original.Length];
+                Array.Copy(original, reset,original.Length);
+                return reset;
+            }
+
+            Random random = new Random();
+            /** Returns a random shuffling of the array. */
+            public int[] Shuffle()
+            {
+                if (original == null) return null;
+                var shuffle = new int[original.Length];
+                Array.Copy(original, shuffle, original.Length);
+
+                for (int i = 0; i < shuffle.Length; i++)
+                {
+                    int j = random.Next(i, shuffle.Length); 
+                    var temp = shuffle[i];
+                    shuffle[i] = shuffle[j];
+                    shuffle[j] = temp;
+                }
+
+                return shuffle;
+            }
+        }
+
+        /**
+         * Your Solution object will be instantiated and called as such:
+         * Solution obj = new Solution(nums);
+         * int[] param_1 = obj.Reset();
+         * int[] param_2 = obj.Shuffle();
+         */
+
+        [Test]
+        public void TestSample1()
+        {
+            var array = new int[] {1, 2, 3};
+            var solution = new Solution(array);
+            Assert.AreEqual(array,solution.Reset());
+        }
+        [Test]
+        public void TestSample2()
+        {
+            var array = new int[] { 1, 2, 3 };
+            var solution = new Solution(array);
+            Assert.AreEqual(array, solution.Reset());
+            Assert.AreNotEqual(array, solution.Shuffle());
+            Assert.AreEqual(array, solution.Reset());
+        }
+
+        public string IntarrayToString(int[] array)
+        {
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                s.Append(array[i]);
+            }
+
+            return s.ToString();
+        }
+        [Test]
+        public void TestSample3()
+        {
+            var array = new int[] { 1, 2, 3 };
+            var dic = new string[]
+            {"123","132","213","231","321","312"
+            };
+            var p = new int[6];
+            var solution = new Solution(array);
+            for (int j = 0; j < 1000; j++)
+            {
+                var shuffle = solution.Shuffle();
+                for (int i = 0; i < dic.Length; i++)
+                {
+                    if (dic[i].Equals(IntarrayToString(shuffle))) p[i]++;
+                }
+            }
+            Assert.IsNotNull(p);
+        }
+    }
+}
---
+++ LeetCodeRush/Simple/Design/最小栈.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeRush.Simple.Design
+{
+    [TestFixture]
+    public class 最小栈
+    {
+        public class MinStack
+        {
+            private readonly Stack<int> data = new Stack<int>();
+            private readonly Stack<int> minValue = new Stack<int>();
+
+            /** initialize your data structure here. */
+            public MinStack()
+            {
+
+            }
+
+            public void Push(int x)
+            {
+                data.Push(x);
+                if (minValue.Count == 0 || x <= minValue.Peek())
+                    minValue.Push(x);
+            }
+
+            public void Pop()
+            {
+                int value = data.Pop();
+                if (value == minValue.Peek())
+                    minValue.Pop();
+            }
+
+            public int Top()
+            {
+                return data.Peek();
+            }
+
+            public int GetMin()
+            {
+                return minValue.Peek();
+            }
+        }
+
+        /**
+         * Your MinStack object will be instantiated and called as such:
+         * MinStack obj = new MinStack();
+         * obj.Push(x);
+         * obj.Pop();
+         * int param_3 = obj.Top();
+         * int param_4 = obj.GetMin();
+         */
+         
+    }
+}
