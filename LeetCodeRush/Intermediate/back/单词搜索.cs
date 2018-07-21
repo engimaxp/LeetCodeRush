6b1a90bcda030d520652afc22c6d24a29b99fe3a
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-07-21 14:42:01 +0800

    mid backtrace 2/2

---
+++ LeetCodeRush/Intermediate/back/单词搜索.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeRush.Intermediate.back
+{
+    [TestFixture]
+    public class 单词搜索
+    {
+        public class Solution
+        {
+            public bool Exist(char[,] board, string word)
+            {
+                if (board == null || string.IsNullOrEmpty(word)) return false;
+                int m = board.GetLength(0);
+                int n = board.GetLength(1);
+                if (m <= 0 || n <= 0) return false;
+                bool[,] visited = new bool[m,n];
+                for (int j = 0; j < m; j++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (Dfs(word, 0, j, i, board, visited, m - 1, n - 1))
+                        {
+                            return true;
+                        }
+                        visited = new bool[m, n];
+                    }
+                }
+
+                return false;
+            }
+
+            private bool Dfs(string word, int index, int row, int col, char[,] board,bool[,] visited,int maxrow,int maxcol)
+            {
+                if (board[row, col] == word[index])
+                {
+                    visited[row, col] = true;
+                    if (index == word.Length - 1)
+                    {
+                        //find
+                        return true;
+                    }
+                    if(row-1>=0&& !visited[row - 1, col])
+                        if(Dfs(word,index+1,row-1,col,board,(bool[,])visited.Clone(),maxrow,maxcol)) return true;
+
+                    if ( row + 1 <= maxrow&& !visited[row + 1, col])
+                        if (Dfs(word, index + 1, row + 1, col, board, (bool[,])visited.Clone(), maxrow, maxcol)) return true;
+
+                    if ( col - 1 >= 0&& !visited[row, col - 1])
+                        if (Dfs(word, index + 1, row, col - 1, board, (bool[,])visited.Clone(), maxrow, maxcol)) return true;
+
+                    if ( col+1 <= maxcol&& !visited[row, col + 1])
+                        if (Dfs(word, index + 1, row, col + 1, board, (bool[,])visited.Clone(), maxrow, maxcol)) return true;
+
+                }
+
+                return false;
+            }
+        }
+        [TestCase("ABCCED",ExpectedResult = true)]
+        [TestCase("SEE", ExpectedResult = true)]
+        [TestCase("ABCB", ExpectedResult = false)]
+        public bool TestMethod(string word)
+        {
+            var result = new Solution().Exist(new[,]
+            {
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                { 'A', 'D', 'E', 'E'}
+            }, word);
+            return result;
+        }
+        [TestCase("AAB", ExpectedResult = true)]
+        public bool TestMethod2(string word)
+        {
+            var result = new Solution().Exist(new[,]
+            {
+                {'C', 'A', 'A'},
+                {'A', 'A', 'A'},
+                {'B', 'C', 'D'}
+            }, word);
+            return result;
+        }
+        [TestCase("ABCESEEEFS", ExpectedResult = true)]
+        public bool TestMethod3(string word)
+        {
+            var result = new Solution().Exist(new[,]
+            {
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'E', 'S'},
+                {'A', 'D', 'E', 'E'}
+            }, word);
+            return result;
+        }
+    }
+}
---
+++ LeetCodeRush/Intermediate/back/子集.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeRush.Intermediate.back
+{
+    [TestFixture]
+    public class 子集
+    {
+        public class Solution
+        {
+            public IList<IList<int>> Subsets(int[] nums)
+            {
+                var result = new List<IList<int>>(){new List<int>()};
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    var result2 = new List<IList<int>>();
+                    result.ForEach(x=>result2.Add(x.ToList()));
+                    result2.ForEach(x=>x.Add(nums[i]));
+                    result.AddRange(result2);
+                }
+
+                return result;
+            }
+        }
+        [Test]
+        public void TestMethod()
+        {
+            var result = new Solution().Subsets(new int[]{ 1, 2, 3 });
+            Assert.IsNotNull(result);
+        }
+        
+    }
+}
