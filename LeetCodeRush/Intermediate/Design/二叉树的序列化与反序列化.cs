d619a4cdaf93fba5754c610846f31b4e74076351
DESKTOP-D3K0007\epevans <wxf_072445@126.com>
2018-07-26 15:38:46 +0800

    mid design 1/1

---
+++ LeetCodeRush/Intermediate/Design/二叉树的序列化与反序列化.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCodeRush.Intermediate.Design
+{
+    [TestFixture]
+    public class 二叉树的序列化与反序列化
+    {
+
+        public class TreeNode
+        {
+            public TreeNode left;
+            public TreeNode right;
+            public int val;
+
+            public TreeNode(int x)
+            {
+                val = x;
+            }
+
+            public static TreeNode ConstructFromArray(int?[] array)
+            {
+                var root = new TreeNode(0);
+                return Create_tree(root, array, 0);
+            }
+
+            private static TreeNode Create_tree(TreeNode node, int?[] a, int index)
+            {
+                if (index >= a.Length)
+                    return null;
+                if (a[index].HasValue)
+                {
+                    node = new TreeNode(a[index].Value);
+                    node.left = Create_tree(node.left, a, 2 * index + 1);
+                    node.right = Create_tree(node.right, a, 2 * index + 2);
+                }
+                else
+                {
+                    node = null;
+                }
+
+                return node;
+            }
+        }
+
+        public class Codec
+        {
+            // 前序中序遍历确定一颗二叉树
+            public string Serialize(TreeNode root)
+            {
+                    if (root == null)
+                        return "";
+                    if (root.left == null && root.right == null)
+                        return root.val + "";
+                    if (root.right == null)
+                        return root.val + "(" + Serialize(root.left) + ")";
+                    return root.val + "(" + Serialize(root.left) + ")(" + Serialize(root.right) + ")";
+            }
+
+            // Decodes your encoded data to tree.
+            public TreeNode Deserialize(string data)
+            {
+                if (string.IsNullOrEmpty(data)) return null;
+                var leftTreeStart = data.IndexOf("(",StringComparison.CurrentCultureIgnoreCase);
+                if(leftTreeStart<0) return new TreeNode(int.Parse(data));
+                int pos = 1;
+                int leftTreeEnd = leftTreeStart;
+                var result = new TreeNode(int.Parse(data.Substring(0,leftTreeStart)));
+
+                while (pos >0 && leftTreeEnd<data.Length)
+                {
+                    leftTreeEnd++;
+                    if (data[leftTreeEnd] == '(')
+                        pos++;
+                    if (data[leftTreeEnd] == ')')
+                        pos--;
+                }
+                result.left = Deserialize(data.Substring(leftTreeStart+1, leftTreeEnd - leftTreeStart -1));
+                if (leftTreeEnd < data.Length - 1)
+                {
+                    result.right = Deserialize(data.Substring(leftTreeEnd+2, data.Length - leftTreeEnd -3));
+                }
+
+                //用递归的方法来实现，第一个值为根节点，第一个括号到其对应的右括号A位置中间的内容为左子节点，A位置+1到结尾为右子节点
+                return result;
+            }
+        }
+        
+        [Test]
+        public void TestMethods()
+        {
+            var root = TreeNode.ConstructFromArray(new int?[] {1, 2, 3, null, null, 4, 5});
+            
+            Codec codec = new Codec();
+            var serlizeresult = codec.Serialize(root);
+            var result = codec.Deserialize(serlizeresult);
+
+            Assert.IsNotNull(result);
+        }
+
+
+        [Test]
+        public void TestMethods2()
+        {
+            var root = TreeNode.ConstructFromArray(new int?[] {1, 10, null, 9, 11, null, 8, 12, null, 7, null, null, 13, null, 6, 14, null, 5, null, null, 15, null, 4, 16, null, 3, null, null, 17, null, 2, 18, null, 1, null, null, 19 });
+
+            Codec codec = new Codec();
+            var serlizeresult = codec.Serialize(root);
+            var result = codec.Deserialize(serlizeresult);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ LeetCodeRush/Intermediate/Design/插入删除获取随机数.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCodeRush.Intermediate.Design
+{
+    [TestFixture]
+    public class 插入删除获取随机数
+    {
+
+        public class RandomizedSet
+        {
+            private List<int> list { get; set; } = new List<int>();
+
+            private Dictionary<int,int> numsMap { get; set; } = new Dictionary<int, int>();
+
+            private Random random;
+            /** Initialize your data structure here. */
+            public RandomizedSet()
+            {
+                random = new Random();
+            }
+
+            /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+            public bool Insert(int val)
+            {
+                if (numsMap.ContainsKey(val)) return false;
+                list.Add(val);
+                numsMap[val] = list.Count-1;
+                return true;
+            }
+
+            /** Removes a value from the set. Returns true if the set contained the specified element. */
+            public bool Remove(int val)
+            {
+                if (!numsMap.ContainsKey(val)) return false;
+                var last = list[list.Count - 1];
+                list[list.Count - 1] = list[numsMap[val]];
+                list[numsMap[val]] = last;
+                numsMap[last] = numsMap[val];
+                list.RemoveAt(list.Count-1);
+                numsMap.Remove(val);
+                return true;
+            }
+
+            /** Get a random element from the set. */
+            public int GetRandom()
+            {
+                return list[random.Next(0, list.Count)];
+            }
+        }
+
+        /**
+         * Your RandomizedSet object will be instantiated and called as such:
+         * RandomizedSet obj = new RandomizedSet();
+         * bool param_1 = obj.Insert(val);
+         * bool param_2 = obj.Remove(val);
+         * int param_3 = obj.GetRandom();
+         */
+        [Test]
+        public void TestMethod()
+        {
+            RandomizedSet obj = new RandomizedSet();
+            Assert.IsTrue(obj.Insert(1));
+            Assert.IsTrue(obj.Insert(2));
+
+            Assert.IsFalse(obj.Insert(2));
+
+            Assert.IsTrue(obj.Remove(1));
+            Assert.IsFalse(obj.Remove(3));
+
+            Assert.IsTrue(obj.Insert(1));
+            Assert.IsTrue(obj.Insert(3));
+            Assert.IsTrue(obj.Insert(4));
+            Assert.IsTrue(obj.Insert(5));
+            Assert.IsTrue(obj.Insert(6));
+
+            int[] data = new int[6];
+            for (int i = 0; i < 3000; i++)
+            {
+                data[obj.GetRandom() - 1]++;
+            }
+            Assert.IsNotNull(data);
+        }
+    }
+}
